5d1d8b66fb85e5def81e923fbbde8cb6e202ee64
Gabriel Orbeli <gabriel.orbeli@gmail.com>
2022-10-27 20:17:00 -0300

    chore: use dinamic env path

--- project/alembic/env.py
+++ project/alembic/env.py
@@ -11,7 +11,6 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 sys.path.append(BASE_DIR)
-print(os.environ["DATABASE_URL"])
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +18,7 @@ config = context.config
 
 # this will overwrite the ini-file sqlalchemy.url path
 # with the path given in the config of the main code
-config.set_main_option("sqlalchemy.url", "postgresql://postgres:postgres@db/test_db")
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
